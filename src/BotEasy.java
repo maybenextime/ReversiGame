22f81db19dcd5709297480fffb018d84a11f28d2
maybenextime <whysobeos@gmail.com>
2018-12-14 03:07:56 +0300

    add miniMax algorithm for botHard with depth=5

--- src/BotEasy.java
+++ src/BotEasy.java
@@ -1,13 +1,40 @@
+import java.util.ArrayList;
 import java.util.List;
-import java.util.Random;
+import java.util.*;
 
 public class BotEasy implements Bot {
     private GamePlay gamePlay;
     private List<CoordValue.Coord> listPosMove;
+    private static  final int[][] boardValue = new int[][]{
+            {100, -1, 5, 2, 2, 5, -1, 100},
+            {-1, -20, 1, 1, 1, 1, -20, -1},
+            {5, 1, 1, 1, 1, 1, 1, 5},
+            {2, 1, 1, 0, 0, 1, 1, 2},
+            {2, 1, 1, 0, 0, 1, 1, 2},
+            {5, 1, 1, 1, 1, 1, 1, 5},
+            {-1, -20, 1, 1, 1, 1, -20, -1},
+            {100, -1, 5, 2, 2, 5, -1, 100},
+    };
 
     @Override
     public CoordValue.Coord move() {
-        return listPosMove.get(new Random().nextInt(listPosMove.size()));
+        int highestScore = -20;
+        CoordValue.Coord goodMove;
+        List<CoordValue.Coord> listOfGoodMove = new ArrayList<>();
+        for (CoordValue.Coord move : listPosMove) {
+            if (boardValue[move.getRow()][move.getCol()] >= highestScore) {
+                if (boardValue[move.getRow()][move.getCol()] == highestScore) {
+                    listOfGoodMove.add(move);
+                } else {
+                    listOfGoodMove.clear();
+                    listOfGoodMove.add(move);
+                }
+                highestScore = boardValue[move.getRow()][move.getCol()];
+            }
+        }
+        new Random().nextInt(listPosMove.size());
+        goodMove = listOfGoodMove.get(new Random().nextInt(listOfGoodMove.size()));
+        return goodMove;
     }
 
     @Override
@@ -15,6 +42,4 @@ public class BotEasy implements Bot {
         this.gamePlay = game;
         this.listPosMove = game.moveList(game.currentBoard, game.currentColor);
     }
-
-
 }
